59105fc9e38a36b16e947bef8e78c95b0f8c8427
Sheikh Musalin <sheikhmusalin@gmail.com>
2023-04-10 15:55:05 -0400

    Split Data

--- ratings.py
+++ ratings.py
@@ -2,3 +2,10 @@
 
 
 # put your code here
+file_ratings = open("scores.txt")
+
+for line in file_ratings:
+    # split_line = line.rsplit()
+    name_and_score = line.strip().split(":")
+    # name_and_score = split_line.split(':')
+    print(name_and_score)
